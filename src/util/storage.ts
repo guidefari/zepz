2cca1d356cdd95729cb3cabf4c58ae3e4bf7b065
Guide Fari <guide.fari@flexperto.com>
2023-05-31 19:06:24 +0200

    refactor: convert to class

--- src/util/storage.ts
+++ src/util/storage.ts
@@ -1,28 +1,50 @@
+"use client"
 import { FormattedUser } from "@/app/users/route";
 import { openDB } from "idb";
 
-const load = openDB('stack-zepz', 1, {
-    upgrade(db) {
-      db.createObjectStore('users');
-    },
-  });
+class LocalStorage {
+    users: FormattedUser[] = []
+    
+    constructor() {
+        this.read()
+    }
 
-
-export async function read(): Promise<FormattedUser[] | undefined> {
-    const db = await load
-
-    try {
-        const data: FormattedUser[] = await fetch('/users').then((res) => res.json())
-        db.clear('users');
-        data.forEach((user) => db.put('users', user, user.user_id));
-        console.log('db.getAll', db.getAll('users'))
-        return db.getAll('users');
-    } catch (error) {
-        if (await db.count('users') > 0) {
-            return await db.getAll('users');
-        } else {
-            console.log("No data is available");
+    load() {
+        try {
+            if (!window) return
+            
+            return openDB('stack-zepz', 1, {
+                upgrade(db) {
+                  db.createObjectStore('users');
+                },
+              });
+        } catch (error) {
+            console.error(error)
         }
+
+    }
+    
+
+    async read(): Promise<FormattedUser[] | undefined> {
+        const db = await this.load()
+
+        try {
+            if (!db) return
+            const data: FormattedUser[] = await fetch('/users').then((res) => res.json())
+            db.clear('users');
+            data.forEach((user) => db.put('users', user, user.user_id));
+            this.users = await db.getAll('users');
+        } catch (error) {
+            if (db && await db.count('users') > 0) {
+                return await db.getAll('users');
+            } else {
+                console.log("No data is available");
+            }
+        }
+
     }
 
 }
+
+const localStorage = new LocalStorage()
+export default localStorage
