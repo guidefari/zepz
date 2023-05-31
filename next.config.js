da56c025fc595d5a34e1f1d44a61bbb83ac1eb94
Guide Fari <guide.fari@flexperto.com>
2023-05-31 21:44:33 +0200

    chore: remove top level await flag

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   webpack: (config) => {
     // this will override the experiments
-    config.experiments = { ...config.experiments, topLevelAwait: true };
+    config.experiments = { ...config.experiments };
     // this will just update topLevelAwait property of config.experiments
     // config.experiments.topLevelAwait = true 
     return config;
