81af4c8692605437358d1beca4e364ba774be373
Guide Fari <guide.fari@flexperto.com>
2023-06-01 00:11:36 +0200

    styles

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -10,9 +10,9 @@ type Props = {
 export const UserCard = ({ user }: Props) => {
   return (
     <div className="p-6">
-      <div className='relative rounded-xl hover:bg-blue-500 group'>
+      <div className={`relative rounded-xl  ${user.blocked && `ring-4 ring-red-800`} group`}>
         <img
-          className="object-cover object-center w-full mb-8 group-hover:opacity-40 lg:h-48 md:h-36 rounded-xl"
+          className={`object-cover object-center w-full mb-8 ${user.blocked && `opacity-40`}  lg:h-48 md:h-36 rounded-xl`}
           src={user.profile_image}
           alt="blog"
         />
@@ -20,14 +20,12 @@ export const UserCard = ({ user }: Props) => {
         <Dropdown user={user}/>
         </div>
       </div>
-      <div className="inline-flex justify-between w-full">
-        <h1 className="mb-8 text-xl font-semibold leading-none tracking-tighter text-neutral-600">
+        <h1 className="text-xl  font-semibold leading-none tracking-tighter text-neutral-600">
           {user.display_name}
         </h1>
-        <span title='Reputation score'>{new Intl.NumberFormat('en-US').format(user.reputation)}</span>
-        {user.blocked && <span className='text-red-500'>Blocked</span>}
-        {user.following && <span className='text-blue-500'>Following</span>}
-      </div>
+        <h4 title='Reputation score' className='mb-2'>🔥{new Intl.NumberFormat('en-US').format(user.reputation)}</h4>
+        {user.blocked && <div className='text-red-500 inline-block rounded-xl p-3 bg-red-950'>Blocked</div>}
+        {user.following && <div className='text-blue-500 inline-block rounded-xl p-3 bg-blue-950'>Following</div>}
     </div>
   )
 }
