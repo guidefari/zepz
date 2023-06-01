ea2c3f5e0ddf4cd62776779d9a5068e5f375f22b
Guide Fari <guide.fari@flexperto.com>
2023-06-01 21:44:17 +0200

    chore: formatting

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -9,24 +9,32 @@ type Props = {
   user: FormattedUser
 }
 
-
-export const Dropdown = observer(({user}: Props) => {
-  
+export const Dropdown = observer(({ user }: Props) => {
   return (
-  <DropdownMenu.Root>
-    <DropdownMenu.Trigger className='focus:ring-2 ring-inset IconButton'>
+    <DropdownMenu.Root>
+      <DropdownMenu.Trigger className="focus:ring-2 ring-inset IconButton">
         <ThreeDots />
-    </DropdownMenu.Trigger>
+      </DropdownMenu.Trigger>
 
-    <DropdownMenu.Portal>
-      <DropdownMenu.Content className="DropdownMenuContent">
-        <DropdownMenu.Item className={`text-black cursor-pointer ${user.blocked && `cursor-not-allowed`}`} disabled={user?.blocked} onClick={() => store.toggleFollowUser(user?.user_id)}>
-          <button className='text-black cursor-pointer'>{user?.following ? 'unfollow' : 'follow'}</button>
-        </DropdownMenu.Item>
-        <DropdownMenu.Item className='text-black cursor-pointer' onClick={() => store.toggleUserBlock(user?.user_id)}>
-          <button>{user?.blocked ? 'unblock' : 'block'}</button>
-        </DropdownMenu.Item>
-      </DropdownMenu.Content>
-    </DropdownMenu.Portal>
-  </DropdownMenu.Root>
-)})
+      <DropdownMenu.Portal>
+        <DropdownMenu.Content className="DropdownMenuContent">
+          <DropdownMenu.Item
+            className={`text-black cursor-pointer ${user.blocked && `cursor-not-allowed`}`}
+            disabled={user?.blocked}
+            onClick={() => store.toggleFollowUser(user?.user_id)}
+          >
+            <button className="text-black cursor-pointer">
+              {user?.following ? 'unfollow' : 'follow'}
+            </button>
+          </DropdownMenu.Item>
+          <DropdownMenu.Item
+            className="text-black cursor-pointer"
+            onClick={() => store.toggleUserBlock(user?.user_id)}
+          >
+            <button>{user?.blocked ? 'unblock' : 'block'}</button>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Portal>
+    </DropdownMenu.Root>
+  )
+})
